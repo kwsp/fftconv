64d0f49fcca966ca0b31b9d8b818ca32f0723200
tnie <tnie@tuta.io>
2024-07-07 00:41:18 -0500

    updated bench

--- plot_bench.py
+++ plot_bench.py
@@ -14,6 +14,7 @@ class Benchmark:
     param1: int
     param2: int
     cpu_time_ns: int
+    cpu_time_ms: int
     iterations: int
 
     @classmethod
@@ -22,12 +23,14 @@ class Benchmark:
         func_name, params = name.split("/", 1)
         params = params.split("/")
         assert bm["time_unit"] == "ns"
+        time_ns = int(bm["cpu_time"])
         return cls(
             name=name,
             func_name=func_name,
             param1=int(params[0]),
             param2=int(params[1]),
-            cpu_time_ns=int(bm["cpu_time"]),
+            cpu_time_ns=time_ns,
+            cpu_time_ms=int(time_ns/1000),
             iterations=int(bm["iterations"]),
         )
 
@@ -45,18 +48,28 @@ df = pd.DataFrame(bms)
 df.head()
 
 # %%
-df = df[df.func_name.str.endswith("oaconvolve")]
+df = df[df.func_name.str.startswith("BM_oaconvolve")]
+# df = df[df.func_name.str.endswith("naive") == False]
+df
 
 # %%
-for param2 in df["param2"].unique():
-    fig, ax = plt.subplots()
+param2unique = df["param2"].unique()
+assert len(param2unique) == 4 # change subplot otherwise
+fig, axes = plt.subplots(2, 2, figsize=(10, 8), sharex=True)
+for ax, param2 in zip(axes.flatten(), param2unique):
+    # group by param2 (second input to bench range, kernel length)
     groupby = df[df["param2"] == param2].groupby("func_name")
-    groupby.plot(x="param1", y="cpu_time_ns", ax=ax, marker="x", markeredgewidth=2.0)
-    ax.legend(labels=groupby.groups.keys())
+    # plot CPU time
+    groupby.plot(x="param1", y="cpu_time_ms", ax=ax, marker="x", markeredgewidth=2.0)
     ax.set_xlabel("Signal Length")
-    ax.set_ylabel("Time (ns)")
+    ax.set_ylabel("Time (ms)")
+    ax.set_ylim(ymin=0)
+    # Remove BM_prefix from function name for label
+    labels = [k.removeprefix("BM_") for k in groupby.groups.keys()]
+    ax.legend(labels)
     ax.set_title(f"Kernel length = {param2}")
 
-# %%
+fig.suptitle("Overlap-Add Convolution (C++)")
+plt.savefig(f"bench_{date}.svg")
 
 # %%
