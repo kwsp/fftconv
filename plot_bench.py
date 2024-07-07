02d6a808bff36b6a00808c55e96a94f96fefb36a
tnie <tnie@tuta.io>
2024-07-07 00:41:19 -0500

    make plot_bench work with txt output

--- plot_bench.py
+++ plot_bench.py
@@ -1,11 +1,10 @@
 # %%
 from __future__ import annotations
+from pathlib import Path
 from dataclasses import dataclass
 import json
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from pprint import pprint
 
 
 @dataclass
@@ -25,6 +24,7 @@ class Benchmark:
         params = params.split("/")
         assert bm["time_unit"] == "ns"
         time_ns = int(bm["cpu_time"])
+        iterations = int(bm["iterations"])
         return cls(
             name=name,
             func_name=func_name,
@@ -32,24 +32,76 @@ class Benchmark:
             param2=int(params[1]),
             cpu_time_ns=time_ns,
             cpu_time_us=int(time_ns / 1000),
-            iterations=int(bm["iterations"]),
+            iterations=iterations,
+        )
+
+    @classmethod
+    def from_txt(cls, txt) -> Benchmark:
+        parts = txt.split()
+        name = parts[0]
+        func_name, params = name.split("/", 1)
+        params = params.split("/")
+        assert parts[4] == "ns"
+        time_ns = int(parts[3])
+        iterations = int(parts[5])
+        return cls(
+            name=name,
+            func_name=func_name,
+            param1=int(params[0]),
+            param2=int(params[1]),
+            cpu_time_ns=time_ns,
+            cpu_time_us=int(time_ns / 1000),
+            iterations=iterations,
         )
 
 
+result = Path("./build/clang-release/benchmark/result.json")
+
+if result.suffix == ".json":
+    ...
+
+
 # %%
-result_json = "./build/clang-release/benchmark/result.json"
-with open(result_json, "r") as fp:
-    data = json.load(fp)
+#
+# JSON result
+#
+def parse_json_result(path: Path):
+    with open(path, "r") as fp:
+        data = json.load(fp)
 
-context = data["context"]
-date = context["date"].rsplit("-", 1)[0]
-date = date.replace(":", "-")
-benchmark = data["benchmarks"][0]
-# pprint(benchmark)
+    context = data["context"]
+    date = context["date"].rsplit("-", 1)[0]
+    date = date.replace(":", "-")
+    benchmark = data["benchmarks"][0]
 
-bms = [Benchmark.from_json(bm) for bm in data["benchmarks"]]
-df = pd.DataFrame(bms)
-df.head()
+    bms = [Benchmark.from_json(bm) for bm in data["benchmarks"]]
+    return date, pd.DataFrame(bms)
+
+
+# %%
+#
+# Txt results
+#
+def parse_txt_result(path: Path):
+    with open(path, "r") as fp:
+        data = fp.read().strip()
+
+    lines = data.split("\n")
+    it = (i for i, line in enumerate(lines) if line.startswith("---"))
+    deli1, deli2 = next(it), next(it)
+
+    context = lines[:deli1]
+    benchmarks = lines[deli2 + 1 :]
+
+    date = context[0]
+    bms = [Benchmark.from_txt(bm) for bm in benchmarks]
+    return date, pd.DataFrame(bms)
+
+
+date, df = parse_txt_result(Path("./bench_results_20240706_m1.txt"))
+
+# %%
+df
 
 # %%
 df = df[df.func_name.str.startswith(("BM_oaconvolve", "BM_arma_conv"))]
@@ -58,7 +110,7 @@ df
 # %%
 param2unique = df["param2"].unique()
 assert len(param2unique) == 4  # change subplot otherwise
-fig, axes = plt.subplots(2, 2, figsize=(10, 10), sharex=True)
+fig, axes = plt.subplots(2, 2, figsize=(10, 10))
 for ax, param2 in zip(axes.flatten(), param2unique):
     # group by param2 (second input to bench range, kernel length)
     groupby = df[df["param2"] == param2].groupby("func_name")
