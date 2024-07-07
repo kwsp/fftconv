67ebc8c40116155eeff9beece1262f7b28c506f6
tnie <tnie@tuta.io>
2024-07-07 00:41:17 -0500

    use unordered_map

--- include/fftconv.h
+++ include/fftconv.h
@@ -2,9 +2,10 @@
 #define __FFTCONV_H__
 #include <cassert>
 #include <complex>
-#include <map>
+#include <cstring>
 #include <memory>
 #include <shared_mutex>
+#include <unordered_map>
 #include <vector>
 
 #include <fftw3.h>
@@ -20,9 +21,9 @@ struct fftconv_plans {
   fftw_plan backward;
 
   // NOT THREAD-SAFE RIGHT NOW
-  double* real_buf;
-  fftw_complex* complex_buf_a;
-  fftw_complex* complex_buf_b;
+  double *real_buf;
+  fftw_complex *complex_buf_a;
+  fftw_complex *complex_buf_b;
 
   fftconv_plans(size_t padded_length) {
 
@@ -37,10 +38,10 @@ struct fftconv_plans {
     // Compute the plans
     this->forward = fftw_plan_dft_r2c_1d(padded_length, real_buf, complex_buf_a,
                                          FFTW_ESTIMATE);
-    this->backward = fftw_plan_dft_c2r_1d(padded_length, complex_buf_a, real_buf,
-                                          FFTW_ESTIMATE);
-
+    this->backward = fftw_plan_dft_c2r_1d(padded_length, complex_buf_a,
+                                          real_buf, FFTW_ESTIMATE);
   }
+
   ~fftconv_plans() {
     fftw_destroy_plan(forward);
     fftw_destroy_plan(backward);
@@ -54,7 +55,6 @@ struct fftconv_plans {
 // Thread-safe
 class FFTW_PLAN_STORE {
 public:
-
   // Get the fftconv_plans instance for the input size
   // If the instance doesn't exist, create it
   static std::shared_ptr<fftconv_plans> get(size_t size) {
@@ -84,7 +84,7 @@ private:
   }
 
   // cache and mutex
-  std::map<size_t, std::shared_ptr<fftconv_plans>> cache;
+  std::unordered_map<size_t, std::shared_ptr<fftconv_plans>> cache;
   mutable std::shared_mutex mutex_;
 };
 
@@ -98,8 +98,10 @@ vector<T> vector_elementwise_multiply(const vector<T> a, const vector<T> b) {
   return result;
 }
 
-void vector_elementwise_multiply(const fftw_complex *a, const fftw_complex *b,
-                                 const size_t length, fftw_complex *result) {
+inline void vector_elementwise_multiply(const fftw_complex *a,
+                                        const fftw_complex *b,
+                                        const size_t length,
+                                        fftw_complex *result) {
   for (auto i = 0; i < length; ++i) {
     std::complex<double> _a(a[i][0], a[i][1]);
     std::complex<double> _b(b[i][0], b[i][1]);
@@ -121,8 +123,9 @@ void _copy_to_padded_buffer(const T *src, const size_t src_size, T *dst,
 
 // http://en.wikipedia.org/w/index.php?title=Convolution&oldid=630841165#Fast_convolution_algorithms
 // size(a) >= size(b). size(a) must be greater or equal to size(b);
-void convolve1d_ref(const double *a, const size_t a_size, const double *b,
-                    const size_t b_size, double *result) {
+inline void convolve1d_ref(const double *a, const size_t a_size,
+                           const double *b, const size_t b_size,
+                           double *result) {
   // length of the real arrays, including the final convolution output
   size_t padded_length = a_size + b_size - 1;
   // length of the complex arrays
@@ -152,13 +155,6 @@ void convolve1d_ref(const double *a, const size_t a_size, const double *b,
   // Compute Fourier transform of vector b
   fftw_execute_dft_r2c(plan_forward, b_buf, B_buf);
 
-#ifdef DEBUG
-  printf("FFT(a) ");
-  print_complex_array(A_buf, complex_length);
-  printf("FFT(b) ");
-  print_complex_array(B_buf, complex_length);
-#endif
-
   // Compute backward fft plan
   fftw_complex *input_buffer = fftw_alloc_complex(complex_length);
   double *output_buffer = fftw_alloc_real(padded_length);
@@ -170,11 +166,6 @@ void convolve1d_ref(const double *a, const size_t a_size, const double *b,
   vector_elementwise_multiply(
       A_buf, B_buf, complex_length,
       input_buffer); // A_buf becomes input to inverse conv
-
-#ifdef DEBUG
-  printf("element multiply ");
-  print_complex_array(input_buffer, complex_length);
-#endif
 
   fftw_execute_dft_c2r(plan_backward, input_buffer, output_buffer);
 
@@ -193,8 +184,8 @@ void convolve1d_ref(const double *a, const size_t a_size, const double *b,
   fftw_destroy_plan(plan_backward);
 }
 
-vector<double> convolve1d_ref(const vector<double> &a,
-                              const vector<double> &b) {
+inline vector<double> convolve1d_ref(const vector<double> &a,
+                                     const vector<double> &b) {
   int padded_length = a.size() + b.size() - 1;
   vector<double> result(padded_length);
   convolve1d_ref(a.data(), a.size(), b.data(), b.size(), result.data());
@@ -208,7 +199,7 @@ vector<double> convolve1d_ref(const vector<double> &a,
 //    * Reuse buffers (3 fftw_mallocs vs 6 in convolve_ref)
 // https://en.wikipedia.org/w/index.php?title=Convolution#Fast_convolution_algorithms
 inline void convolve1d(const double *a, const size_t a_size, const double *b,
-                const size_t b_size, double *result) {
+                       const size_t b_size, double *result) {
   // length of the real arrays, including the final convolution output
   size_t padded_length = a_size + b_size - 1;
   // length of the complex arrays
@@ -218,11 +209,11 @@ inline void convolve1d(const double *a, const size_t a_size, const double *b,
   auto plans = FFTW_PLAN_STORE::get(padded_length);
 
   // Allocate fftw buffers for a
-  //double *real_buf = fftw_alloc_real(padded_length);
-  //fftw_complex *complex_buf_a = fftw_alloc_complex(complex_length);
-  double* const real_buf = plans->real_buf;
-  fftw_complex* const complex_buf_a = plans->complex_buf_a;
-  fftw_complex* const complex_buf_b = plans->complex_buf_b;
+  // double *real_buf = fftw_alloc_real(padded_length);
+  // fftw_complex *complex_buf_a = fftw_alloc_complex(complex_length);
+  double *const real_buf = plans->real_buf;
+  fftw_complex *const complex_buf_a = plans->complex_buf_a;
+  fftw_complex *const complex_buf_b = plans->complex_buf_b;
 
   // Copy a to buffer
   _copy_to_padded_buffer(a, a_size, real_buf, padded_length);
@@ -231,7 +222,7 @@ inline void convolve1d(const double *a, const size_t a_size, const double *b,
   fftw_execute_dft_r2c(plans->forward, real_buf, complex_buf_a);
 
   // Allocate fftw buffers for b
-  //fftw_complex *complex_buf_b = fftw_alloc_complex(complex_length);
+  // fftw_complex *complex_buf_b = fftw_alloc_complex(complex_length);
 
   // Copy b to buffer. Reuse real buffer
   _copy_to_padded_buffer(b, b_size, real_buf, padded_length);
@@ -256,12 +247,13 @@ inline void convolve1d(const double *a, const size_t a_size, const double *b,
     result[i] = real_buf[i] / padded_length;
   }
 
-  //fftw_free(real_buf);
-  //fftw_free(complex_buf_a);
-  //fftw_free(complex_buf_b);
+  // fftw_free(real_buf);
+  // fftw_free(complex_buf_a);
+  // fftw_free(complex_buf_b);
 }
 
-vector<double> convolve1d(const vector<double> &a, const vector<double> &b) {
+inline vector<double> convolve1d(const vector<double> &a,
+                                 const vector<double> &b) {
   int padded_length = a.size() + b.size() - 1;
   vector<double> result(padded_length);
   convolve1d(a.data(), a.size(), b.data(), b.size(), result.data());
@@ -269,4 +261,4 @@ vector<double> convolve1d(const vector<double> &a, const vector<double> &b) {
 }
 
 } // namespace fftconv
-#endif  // __FFTCONV_H__
+#endif // __FFTCONV_H__
