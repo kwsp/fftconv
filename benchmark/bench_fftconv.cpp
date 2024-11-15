066a11e367b0f959a9c884284d6f27f10822dfff
tnie <tnie@tuta.io>
2024-11-15 14:33:57 -0600

    benchmark use fftw wisdom

--- benchmark/bench_fftconv.cpp
+++ benchmark/bench_fftconv.cpp
@@ -5,6 +5,7 @@
 #include <span>
 
 #include "fftconv.hpp"
+#include "fftw.hpp"
 // #include "fftconv_pocket.hpp"
 
 using std::vector;
@@ -12,6 +13,9 @@ using std::vector;
 // NOLINTBEGIN(*-identifier-length)
 
 //------------------ Helper functions
+
+static void DoSetup(const benchmark::State &state) { fftw::import_wisdom(); }
+static void DoTeardown(const benchmark::State &state) { fftw::export_wisdom(); }
 
 // Wrapper to prevent arma::conv from being optimized away
 // Not storing results back in res.
@@ -82,50 +86,64 @@ void conv_bench_same(benchmark::State &state, Func conv_func) {
   }
 }
 
-const std::vector<std::vector<int64_t>> args{
-    {{1000, 1664, 2304, 2816, 3326, 4352}, {15, 35, 65, 95}}};
+// const std::vector<std::vector<int64_t>> args{
+//     {{1000, 1664, 2304, 2816, 3326, 4352}, {15, 35, 65, 95}}};
+
+const std::vector<std::vector<int64_t>> args{{{2304, 4352}, {65, 95}}};
 
 template <fftconv::FloatOrDouble Real>
 void BM_oaconvolve(benchmark::State &state) {
   conv_bench_full<Real>(state, fftconv::oaconvolve_fftw<Real>);
 }
-BENCHMARK(BM_oaconvolve<double>)->ArgsProduct(args);
-BENCHMARK(BM_oaconvolve<float>)->ArgsProduct(args);
+BENCHMARK(BM_oaconvolve<double>)
+    ->ArgsProduct(args)
+    ->Setup(DoSetup)
+    ->Teardown(DoTeardown);
+BENCHMARK(BM_oaconvolve<float>)
+    ->ArgsProduct(args)
+    ->Setup(DoSetup)
+    ->Teardown(DoTeardown);
 
 template <fftconv::FloatOrDouble Real>
 void BM_oaconvolve_same(benchmark::State &state) {
   conv_bench_same<Real>(state, fftconv::oaconvolve_fftw_same<Real>);
 }
-BENCHMARK(BM_oaconvolve_same<double>)->ArgsProduct(args);
-BENCHMARK(BM_oaconvolve_same<float>)->ArgsProduct(args);
+BENCHMARK(BM_oaconvolve_same<double>)
+    ->ArgsProduct(args)
+    ->Setup(DoSetup)
+    ->Teardown(DoTeardown);
+BENCHMARK(BM_oaconvolve_same<float>)
+    ->ArgsProduct(args)
+    ->Setup(DoSetup)
+    ->Teardown(DoTeardown);
 
-template <fftconv::FloatOrDouble Real>
-void BM_convolve(benchmark::State &state) {
-  conv_bench_full<Real>(state, fftconv::convolve_fftw<Real>);
-}
-BENCHMARK(BM_convolve<double>)->ArgsProduct(args);
-BENCHMARK(BM_convolve<float>)->ArgsProduct(args);
+// template <fftconv::FloatOrDouble Real>
+// void BM_convolve(benchmark::State &state) {
+//   conv_bench_full<Real>(state, fftconv::convolve_fftw<Real>);
+// }
+// BENCHMARK(BM_convolve<double>)->ArgsProduct(args)->Setup(DoSetup)->Teardown(DoTeardown);
+// BENCHMARK(BM_convolve<float>)->ArgsProduct(args)->Setup(DoSetup)->Teardown(DoTeardown);
 
-template <fftconv::FloatOrDouble Real>
-void BM_arma_conv(benchmark::State &state) {
-  conv_bench_full<Real>(state, arma_conv_full<Real>);
-}
-BENCHMARK(BM_arma_conv<double>)->ArgsProduct(args);
-BENCHMARK(BM_arma_conv<float>)->ArgsProduct(args);
+// template <fftconv::FloatOrDouble Real>
+// void BM_arma_conv(benchmark::State &state) {
+//   conv_bench_full<Real>(state, arma_conv_full<Real>);
+// }
+// BENCHMARK(BM_arma_conv<double>)->ArgsProduct(args);
+// BENCHMARK(BM_arma_conv<float>)->ArgsProduct(args);
 
-template <fftconv::FloatOrDouble Real>
-void BM_arma_conv_same(benchmark::State &state) {
-  conv_bench_same<Real>(state, arma_conv_same<Real>);
-}
-BENCHMARK(BM_arma_conv_same<double>)->ArgsProduct(args);
-BENCHMARK(BM_arma_conv_same<float>)->ArgsProduct(args);
+// template <fftconv::FloatOrDouble Real>
+// void BM_arma_conv_same(benchmark::State &state) {
+//   conv_bench_same<Real>(state, arma_conv_same<Real>);
+// }
+// BENCHMARK(BM_arma_conv_same<double>)->ArgsProduct(args);
+// BENCHMARK(BM_arma_conv_same<float>)->ArgsProduct(args);
 
-template <fftconv::FloatOrDouble Real>
-void BM_kfr_conv_same(benchmark::State &state) {
-  conv_bench_same<Real>(state, kfr_conv<Real>);
-}
-BENCHMARK(BM_kfr_conv_same<double>)->ArgsProduct(args);
-BENCHMARK(BM_kfr_conv_same<float>)->ArgsProduct(args);
+// template <fftconv::FloatOrDouble Real>
+// void BM_kfr_conv_same(benchmark::State &state) {
+//   conv_bench_same<Real>(state, kfr_conv<Real>);
+// }
+// BENCHMARK(BM_kfr_conv_same<double>)->ArgsProduct(args);
+// BENCHMARK(BM_kfr_conv_same<float>)->ArgsProduct(args);
 
 // NOLINTEND(*-identifier-length)
 
