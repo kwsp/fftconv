e719b3225d89c164b17e9130310a9181af7684bb
tnie <tnie@tuta.io>
2026-03-10 17:20:19 +0800

    Removed dead code in test_helper

--- test/test_helpers.hpp
+++ test/test_helpers.hpp
@@ -1,11 +1,7 @@
-#include <armadillo>
 #include <cassert>
-#include <cstdio>
 #include <cstdlib>
 #include <fmt/core.h>
-#include <span>
-
-constexpr double Tol = 1e-6;
+#include <functional>
 
 // Run the `callable` `n_runs` times and print the time.
 inline void timeit(const std::string &name,
@@ -18,19 +14,4 @@ inline void timeit(const std::string &name,
   const auto elapsed =
       duration_cast<milliseconds>(high_resolution_clock::now() - start);
   fmt::println("    ({} runs) {} took {}ms", n_runs, name, elapsed.count());
-}
-
-// Compare two vectors
-template <class T1, class T2>
-auto cmp_vec(const std::span<const T1> vec1, const std::span<const T2> vec2)
-    -> bool {
-  assert(vec1.size() == vec2.size());
-  for (auto i = 0; i < vec1.size(); ++i) {
-    if (std::abs(vec1[i] - vec2[i]) > Tol) {
-      printf("Vectors are different: v1[%d]=%f, v2[%d]=%f\n", i, vec1[i], i,
-             vec2[i]);
-      return false;
-    }
-  }
-  return true;
 }
